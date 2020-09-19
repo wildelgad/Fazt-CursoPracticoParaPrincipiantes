43285c0e236582f1d034f3c9454678a52608319e
Wilinton Delgado Bustamante <wildelgad@gmail.com>
2020-09-19 16:01:35 -0500

    Commit 2: Entendiendo State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import './App.css';
 
-function HeyJupiter(props){
-  console.log("este es el: " + props.envioTexto)
-  return(
-    <div id="jupit">
-      <h2>Viajando a Jupiter...</h2>
-      {props.envioTexto}
-      {props.otraProps}
-    </div>
-  )
+// function HeyJupiter(props){
+//   console.log("este es el: " + props.envioTexto)
+//   return(
+//     <div id="jupit">
+//       <h2>Viajando a Jupiter...</h2>
+//       {props.envioTexto}
+//       {props.otraProps}
+//     </div>
+//   )
+// }
+
+class HeyJupiter extends React.Component{
+  
+  state = {
+    show: true,
+  }
+
+  toggleShow = ()=> {
+    this.setState({show: !this.state.show})
+  }
+
+  render(){
+    if(this.state.show){
+      return(
+        <div id="jupit">
+          <h2>Viajando a Jupiter...</h2>
+          {this.props.envioTexto}
+          {this.props.otraProps}
+
+          <button onClick={this.toggleShow}>
+            Toggle Show
+          </button>
+        </div>
+      )
+    }
+    else{
+      return(
+        <div>
+          <h2>No Hay Elementos</h2>
+          <button onClick={this.toggleShow}>
+            Toggle Show
+          </button>
+        </div>
+      )
+    }
+  }
 }
 
 function App() {
