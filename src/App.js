dd0df6151e67774d19b7397e3d97d4aba8fe3155
Wilinton Delgado Bustamante <wildelgad@gmail.com>
2020-09-20 13:45:06 -0500

    Commit 3: iterando un State

--- src/App.js
+++ src/App.js
@@ -1,63 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import tasks from './sample/tasks.json'
 
-// function HeyJupiter(props){
-//   console.log("este es el: " + props.envioTexto)
-//   return(
-//     <div id="jupit">
-//       <h2>Viajando a Jupiter...</h2>
-//       {props.envioTexto}
-//       {props.otraProps}
-//     </div>
-//   )
-// }
+class App extends Component{
 
-class HeyJupiter extends React.Component{
-  
   state = {
-    show: true,
-  }
-
-  toggleShow = ()=> {
-    this.setState({show: !this.state.show})
+    tasks: tasks
   }
 
   render(){
-    if(this.state.show){
-      return(
-        <div id="jupit">
-          <h2>Viajando a Jupiter...</h2>
-          {this.props.envioTexto}
-          {this.props.otraProps}
-
-          <button onClick={this.toggleShow}>
-            Toggle Show
-          </button>
-        </div>
-      )
-    }
-    else{
-      return(
-        <div>
-          <h2>No Hay Elementos</h2>
-          <button onClick={this.toggleShow}>
-            Toggle Show
-          </button>
-        </div>
-      )
-    }
-  }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <h2>Nuevo Titulón</h2>
-        <HeyJupiter envioTexto="- Primer viaje a Jupiter" />
-        <HeyJupiter envioTexto="- Segundo viaje a Jupiter"/>
-        <HeyJupiter otraProps="Aterrizaje forzoso"/>
+    return <div>
+      {this.state.tasks.map(e=> <h1>{e.description}</h1>)}
     </div>
-  );
+  }
 }
 
 export default App;
